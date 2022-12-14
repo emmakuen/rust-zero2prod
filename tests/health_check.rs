d70a268df9ae5314c5096d12e19ada3d49de2ae3
Emma <emmaku@protonmail.com>
2022-12-14 19:30:58 +0900

    reorganize folder structure

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -5,7 +5,7 @@ fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     // retrieve the port assigned by the OS
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let server = zero2prod::startup::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
     // return the app address to the caller
     format!("http://127.0.0.1:{}", port)
