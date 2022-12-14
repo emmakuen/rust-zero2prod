b4be496e2588e60b50e543bd137909dbc28d58a3
Emma <emmaku@protonmail.com>
2022-12-15 00:20:04 +0900

    use logger for telemetry

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
@@ -5,12 +6,17 @@ use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    // initialize logger
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     // panic if config read fails
     let configuration = get_configuration().expect("Failed to read");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres");
+
     let app_address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(app_address)?;
+
     run(listener, connection_pool)?.await
 }
