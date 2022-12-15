5c5f38dcee420c4c3c258893dae514774d68826b
Emma <emmaku@protonmail.com>
2022-12-15 22:50:07 +0900

    incorporate tracing into integration tests

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,15 +1,26 @@
+use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
 use uuid::Uuid;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 pub struct TestApp {
     pub address: String,
     pub connection_pool: PgPool,
 }
 
+// Ensure that the `tracing` stack is only initialized once using `once_cell`
+static TRACING: Lazy<()> = Lazy::new(|| {
+    // initialize tracing
+    let subscriber = get_subscriber("test".into(), "debug".into());
+    init_subscriber(subscriber);
+});
+
 // launch app in the background
 async fn spawn_app() -> TestApp {
+    Lazy::force(&TRACING);
+
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     // retrieve the port assigned by the OS
     let port = listener.local_addr().unwrap().port();
