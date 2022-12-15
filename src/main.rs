19d46c2a08b74b642c45a0e860bb796c843a4e9d
Emma <emmaku@protonmail.com>
2022-12-15 22:22:15 +0900

    use tracing for log

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,34 @@
-use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    // redirect all `log`'s events to our subscriber
+    LogTracer::init().expect("Failed to set logger");
+
     // initialize logger
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    // if RUST_LOG environment variable hasn't been set, fall back to printing all spans at info level or above
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new(
+        "zero2prod".into(),
+        // output the formatted spans to stdout
+        std::io::stdout,
+    );
+
+    // `with` method is provided by `SubscriberExt`, an extension trait for `Subscriber` exposed by `tracing_subscriber`
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+
+    // the following function can be used to specified what subscriber to be used to process spans
+    set_global_default(subscriber).expect("Failed to set subscriber");
 
     // panic if config read fails
     let configuration = get_configuration().expect("Failed to read");
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,7 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -13,6 +14,25 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     connection_pool: web::Data<PgPool>,
 ) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+    // create an info level span that represents the whole HTTP request
+    // tracing allows us to associate structured information to our spans as a collection of key-value pairs.
+    // % symbol is used to tell tracing to use its Display implementation for logging
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    );
+
+    // activate the span using enter method
+    // the following guard will be dropped at the end of the current function, that's when we exit the span
+    let _request_span_guard = request_span.enter();
+
+    // we don't call `.enter` on query_span
+    // `.instrument` takes care of it at the right moments in the query future lifetime
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
+
     let result = sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -24,13 +44,19 @@ pub async fn subscribe(
         Utc::now()
     )
     // 'get_ref' is used here to get an immutable reference to the 'PgConnection' wrapped by 'web::Data'
-    .execute(connection_pool.get_ref())
+    .execute(connection_pool.as_ref())
+    // first we attach the instrumentation to the query span, then we await it
+    .instrument(query_span)
     .await;
 
     match result {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            tracing::info!("request_id {request_id} - New subscriber details have been saved");
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            // error log falls outside of query span - fix it later
+            tracing::error!("request_id {request_id} - Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
