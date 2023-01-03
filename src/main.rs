a24147d6554fd879df044f9814fbde44ec1344ca
Emma <emmaku@protonmail.com>
2023-01-03 13:51:39 +0900

    update configuration

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -12,11 +12,15 @@ async fn main() -> std::io::Result<()> {
 
     // panic if config read fails
     let configuration = get_configuration().expect("Failed to read");
-    let connection_pool =
-        PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
-            .expect("Failed to connect to Postgres");
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.database.connection_string().expose_secret())
+        .expect("Failed to connect to Postgres");
 
-    let app_address = format!("127.0.0.1:{}", configuration.application_port);
+    let app_address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(app_address)?;
 
     run(listener, connection_pool)?.await
